7bdc90aaf1600d88db204bbf852618d406419d17
iamdenizduman <denizduman28@icloud.com>
2025-06-03 01:33:01 +0300

    code - 1

---
+++ OrderService/src/Order.Domain/AggregateModels/OrderModels/Order.cs
@@ -0,0 +1,14 @@
+﻿using Order.Domain.SeedWork;
+
+namespace Order.Domain.AggregateModels.OrderModels
+{
+    public class Order : BaseEntity, IAggregateRoot
+    {
+        public DateTime OrderDate { get; set; }
+        public string Description { get; set; }
+        public int BuyerId { get; set; }
+        public string OrderStatus{ get; set; }
+        public Address Address { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ OrderService/src/Order.Domain/AggregateModels/OrderModels/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿using Order.Domain.SeedWork;
+
+namespace Order.Domain.AggregateModels.OrderModels
+{
+    public class OrderItem : BaseEntity
+    {
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int ProductId { get; set; }
+    }
+}
